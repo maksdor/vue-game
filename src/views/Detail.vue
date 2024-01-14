c655ecefb0ece7fc55cae48409306f39e00014d7
maksdor <dorohin.maksim7474@mail.ru>
2024-01-14 21:07:19 +0300

    asdasdasdasfafdg

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,5 +1,51 @@
+<script setup>
+import * as api from "../api/api.js"
+import {useRoute} from "vue-router"
+import {onMounted, ref} from "vue"
+
+const detail = ref()
+const route = useRoute()
+
+onMounted(async() => {
+    detail.value = await api.DetailGame(route.params.id)
+    console.log("route", route)
+})
+</script>
+
+
 <template>
-    <section>
-        <h1>Detail</h1>
+    <section v-if="detail">
+        <div>
+            <nav>
+                <img
+                    v-for="(scren, index) in detail.screenshots"
+                    :src="scren.image"
+                    :key="index"
+                />
+            </nav>
+            <p>{{detail.description}}</p>
+        </div>
     </section>
 </template>
+<style scoped>
+div {
+    padding: 20px;
+}
+nav {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    grid-gap: 50px;
+}
+img {
+    width: 40%;
+    aspect-ratio: 16/9;
+}
+p {
+    font-size: 24px;
+    margin-top: 20px;
+    color: #aaa;
+    text-align: center;
+}
+
+</style>
--- src/views/RPG.vue
+++ src/views/RPG.vue
@@ -1,5 +1,39 @@
+<script setup>
+import GameCard from "../components/GameCard.vue"
+import * as api from "../api/api.js"
+import {onMounted, ref} from "vue"
+const games = ref()
+onMounted(async () => {
+    games.value = await api.GamesCategory("action-rpg")
+})
+</script>
+
 <template>
     <section>
-        <h1>RPG</h1>
+        <h1 class = 'title'>Action-RPG</h1>
+        <div class = 'block'>
+            <GameCard
+            v-for="(item, index) in games"
+            :game="item"
+            :key="index"
+            />
+        </div>
     </section>
 </template>
+
+<style scoped>
+
+.title {
+    font-size: 40px;
+    text-align: center;
+    margin: 20px 0;
+    color: white;
+}
+.block {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 50px;
+    margin: 0 50px;
+}
+
+</style>
